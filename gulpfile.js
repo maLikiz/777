b3035fda4d8b0357362554644b87936bdf1c7a4d
Nikita Malykh <malixzu@gmail.com>
2019-11-22 23:27:08 +0500

    Add proxy

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,39 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync');
+  browserSync = require('browser-sync');
+proxy = require('proxy-middleware');
+url = require('url');
 
-gulp.task('browser-sync', function () {
-    browserSync({
-        server: {
-            baseDir: 'src'
-        },
-        notify: false,
-        ghostMode: {
-            scroll: false
-        }
-    });
+gulp.task('browser-sync', function() {
+  const proxyHost = 'https://admiral777.vip';
+
+  const api = [
+    '/core/',
+  ];
+
+  const proxyOptionsList = api.map(item => {
+    const proxyOptions = url.parse(`${proxyHost}/${item}`);
+    proxyOptions.route = item;
+
+    return proxy(proxyOptions);
+  });
+
+  browserSync({
+    server: {
+      baseDir: 'src',
+      middleware: proxyOptionsList,
+    },
+    notify: false,
+    ghostMode: {
+      scroll: false,
+    },
+  });
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/**/*.html', browserSync.reload);
-    gulp.watch('src/css/**/*.css', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
-    gulp.watch('src/**/*.php').on('change', browserSync.reload);
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', browserSync.reload);
+  gulp.watch('src/css/**/*.css', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch('src/**/*.php').on('change', browserSync.reload);
 });
 
 gulp.task('default', gulp.parallel(['browser-sync', 'watch']));
